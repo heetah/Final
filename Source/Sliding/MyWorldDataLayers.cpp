37889e700d24aaa3def0fcc0dec82497b2cd678f
盧孟煜 <kyle911125@alum.ccu.edu.tw>
2024-05-12 04:09:37 +0800

    05/12-6

---
+++ Source/Sliding/MyWorldDataLayers.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#pragma once
+extern int time;
+
+#include "MyWorldDataLayers.h"
+int time = 100;
---
+++ Source/Sliding/MyWorldDataLayers.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WorldPartition/DataLayer/WorldDataLayers.h"
+#include "MyWorldDataLayers.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLIDING_API AMyWorldDataLayers : public AWorldDataLayers
+{
+	GENERATED_BODY()
+	
+};
